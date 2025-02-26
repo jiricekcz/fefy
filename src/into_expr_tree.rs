34fc7c49a72072796bc037f26ebf938988383a37
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-26 22:08:51 +0100

    unary and shunting yard

--- src/into_expr_tree.rs
+++ src/into_expr_tree.rs
@@ -1,10 +1,10 @@
-use std::{collections::HashMap, ops::Index};
+use std::{collections::HashMap, fmt::Display, ops::Index};
 
 use anyhow::{anyhow, Result};
 use fef::v0::{
     expr::{
-        Expr, ExprBinaryFloat64Literal, ExprSignedIntLiteral, ExprTree, ExprUnsignedIntLiteral,
-        ExprVariable,
+        Expr, ExprBinaryFloat64Literal, ExprNegation, ExprSignedIntLiteral, ExprTree,
+        ExprUnsignedIntLiteral, ExprVariable,
     },
     raw::VariableLengthEnum,
 };
@@ -16,7 +16,27 @@ pub(crate) fn into_expr_tree(
     variables: &mut Vec<String>,
     in_parenthesis: bool,
 ) -> Result<ExprTree> {
-    let mut symbols: Vec<Option<Symbol>> = Vec::new();
+    // Convert the tokens into symbols
+    let mut symbols = into_symbols(tokens, variables, in_parenthesis)?
+        .into_iter()
+        .map(|s| Some(s))
+        .collect::<Vec<_>>();
+
+    // Removes all unary operators by composing them with their operands
+    compose_unary_expressions(&mut symbols)?;
+
+    // Binary operators - they have the second highest precedence (no others are defined)
+
+    todo!()
+}
+
+/// Converts a sequence of tokens into a sequence of symbols.
+fn into_symbols(
+    tokens: &mut impl Iterator<Item = Result<ParsedToken>>,
+    variables: &mut Vec<String>,
+    in_parenthesis: bool,
+) -> Result<Vec<ParsedSymbol>> {
+    let mut symbols: Vec<ParsedSymbol> = Vec::new();
 
     while let Some(parsed_token) = tokens.next() {
         let parsed_token = parsed_token?;
@@ -86,27 +106,163 @@ pub(crate) fn into_expr_tree(
             }
         };
 
-        symbols.push(Some(symbol));
+        symbols.push(ParsedSymbol {
+            end: parsed_token.end,
+            start: parsed_token.start,
+            symbol,
+        });
     }
-
-    // Unary operators + expr check - they have the highest precedence
-    let mut expecting = Expecting::Operand;
-    let mut unary_operator_read: Option<Operator> = None;
-    for i in 0..symbols.len() {}
-    // Binary operators - they have the second highest precedence (no others are defined)
-    todo!()
+    Ok(symbols)
 }
 
+/// Treats all operators in place of unary operators as unary operators with the same precedence and composes them with their operands, if possible.
+fn compose_unary_expressions(symbols: &mut Vec<Option<ParsedSymbol>>) -> Result<()> {
+    // What is expected in a sequence without unary operators
+    let mut expecting = Expecting::Operand;
+
+    let mut unary_operator_stack: Vec<ParsedOperator> = Vec::new();
+    for i in 0..symbols.len() {
+        let parsed_symbol = symbols[i].take().expect("Symbol vec not clean");
+        match parsed_symbol.symbol {
+            Symbol::Operator(op) => match expecting {
+                Expecting::Operand => {
+                    unary_operator_stack.push(ParsedOperator {
+                        operator: op,
+                        start: parsed_symbol.start,
+                        end: parsed_symbol.end,
+                    });
+                }
+                Expecting::Operator => {
+                    expecting = Expecting::Operand;
+                    symbols[i] = Some(parsed_symbol);
+                }
+            },
+            Symbol::Operand(_) => match expecting {
+                Expecting::Operand => {
+                    let end: usize = parsed_symbol.end;
+                    let mut start: usize = parsed_symbol.start;
+                    let mut expr_to_wrap = match parsed_symbol.symbol {
+                        Symbol::Operand(expr) => expr,
+                        _ => unreachable!(),
+                    };
+
+                    for unary_operator in unary_operator_stack.iter().rev() {
+                        start = unary_operator.start;
+                        expr_to_wrap = wrap(expr_to_wrap, *unary_operator)?;
+                    }
+                    expecting = Expecting::Operator;
+                    symbols[i] = Some(ParsedSymbol {
+                        end,
+                        start,
+                        symbol: Symbol::Operand(expr_to_wrap),
+                    });
+                }
+                Expecting::Operator => {
+                    return Err(anyhow!(
+                        "Expected operator at {}-{} found expression",
+                        parsed_symbol.start,
+                        parsed_symbol.end
+                    ));
+                }
+            },
+        }
+    }
+    Ok(to_cleaned_symbols(symbols))
+}
+
+/// Composes all binary expressions in the sequence of symbols
+fn compose_binary_expressions(symbols: Vec<Option<ParsedSymbol>>) -> Result<ExprTree> {}
+
+/// Converts a sequence of symbols in infix notation to postfix notation
+fn shunting_yard_algorithm(symbols: Vec<ParsedSymbol>) -> Result<Vec<ParsedSymbol>> {
+    let mut output: Vec<ParsedSymbol> = Vec::new();
+    let mut operator_stack: Vec<ParsedOperator> = Vec::new();
+
+    for parsed_symbol in symbols {
+        match parsed_symbol.symbol {
+            Symbol::Operand(_) => output.push(parsed_symbol),
+            Symbol::Operator(op) => {
+                let precedence = op.binary_precedence().ok_or(anyhow!(
+                    "Illegal use of operator \"{}\" as a binary operator at {}-{}",
+                    op,
+                    parsed_symbol.start,
+                    parsed_symbol.end
+                ))?;
+
+                while let Some(&top) = operator_stack.last() {
+                    while top
+                        .operator
+                        .binary_precedence()
+                        .expect("Illegal operator on shunting yard stack")
+                        >= precedence
+                    {
+                        output.push(ParsedSymbol {
+                            start: top.start,
+                            end: top.end,
+                            symbol: Symbol::Operator(top.operator),
+                        });
+                        operator_stack.pop();
+                    }
+                }
+            }
+        }
+    }
+
+    for operator in operator_stack.iter().rev() {
+        output.push(ParsedSymbol {
+            start: operator.start,
+            end: operator.end,
+            symbol: Symbol::Operator(operator.operator),
+        });
+    }
+
+    Ok(output)
+}
+
+/// Removes all `None` values from a vector of `Option`s
+fn to_cleaned_symbols<S>(symbols: &mut Vec<Option<S>>) -> () {
+    symbols.retain(|s| s.is_some());
+}
+
+fn wrap(expr: ExprTree, unary_operator: ParsedOperator) -> Result<ExprTree> {
+    match unary_operator.operator {
+        Operator::Plus => Ok(expr),
+        Operator::Minus => {
+            let expr_obj = ExprNegation::from(expr);
+            Ok(ExprTree::from(Expr::Negation(expr_obj)))
+        }
+        _ => {
+            return Err(anyhow!(
+                "Illegal use of \"{}\" as unary operator at {}-{}",
+                unary_operator.operator,
+                unary_operator.start,
+                unary_operator.end
+            ))
+        }
+    }
+}
+#[derive(Debug, PartialEq, Eq)]
 enum Expecting {
     Operator,
     Operand,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct ParsedOperator {
+    operator: Operator,
+    start: usize,
+    end: usize,
+}
+struct ParsedSymbol {
+    symbol: Symbol,
+    start: usize,
+    end: usize,
+}
 pub(crate) enum Symbol {
     Operand(ExprTree),
     Operator(Operator),
 }
-
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub(crate) enum Operator {
     Plus,
     Minus,
@@ -124,4 +280,39 @@ pub(crate) enum Operator {
     GreaterThan,
     GreaterThanOrEqual,
     Period,
+}
+
+impl Display for Operator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Operator::Plus => "+",
+            Operator::Minus => "-",
+            Operator::Asterisk => "*",
+            Operator::Slash => "/",
+            Operator::DoubleSlash => "//",
+            Operator::Backslash => "\\",
+            Operator::Percent => "%",
+            Operator::Caret => "^",
+            Operator::Comma => ",",
+            Operator::Equals => "==",
+            Operator::NotEquals => "!=",
+            Operator::LessThan => "<",
+            Operator::LessThanOrEqual => "<=",
+            Operator::GreaterThan => ">",
+            Operator::GreaterThanOrEqual => ">=",
+            Operator::Period => ".",
+        };
+        write!(f, "{}", s)
+    }
+}
+impl Operator {
+    pub(crate) fn binary_precedence(&self) -> Option<usize> {
+        match self {
+            Operator::Plus | Operator::Minus => Some(1),
+            Operator::Asterisk | Operator::Slash | Operator::DoubleSlash | Operator::Percent => {
+                Some(2)
+            }
+            _ => None,
+        }
+    }
 }
